a369c915ff6ee1754b795f17261acaedf808602a
“tianchoy” <tianchoy@live.com>
2025-03-07 18:11:30 +0800

    3.7用户中心，存读用户名和头像功能完成

--- cloudfunctions/cl_userInfo/index.js
+++ cloudfunctions/cl_userInfo/index.js
@@ -6,22 +6,36 @@ const db = cloud.database()
 // 云函数入口函数
 exports.main = async (event, context) => {
     const wxContext = cloud.getWXContext()
+    const { type, userAvatar, nickName } = event
     const openid = wxContext.OPENID
-    const  type  = event.type
-    if (type == 'get') {
-        let res = await db.collection('users').where({ openid }).get()
-        if (res.data.length) {
-            return res.data[0]
+    if (type == "get") {
+        // 查询用户
+        let resp = await db.collection("users").where({ openid }).get();
+        if (resp.data.length > 0) {
+            return resp.data[0]; 
         } else {
-          let addData = {
-            nickName:'船连用户',
-            userAvatar:'',
-            openid
-          }
-          let addToSql = await db.collection('users').add({
-              data:addData
-          })
-          return addToSql
+            // 插入新用户
+            let respn = await db.collection("users").add({
+                data: {
+                    nickName: '',
+                    userAvatar: '',
+                    openid
+                }
+            })
+            // 重新查询以确保返回数组
+            res = await db.collection("users").where({ openid }).get();
+            return res.data[0]; // 返回数组
+        }
+    }
+    if (type == 'post') {
+        let respn = await db.collection('users').doc(event.id).update({
+            data: event.fields
+        })
+        resn = await db.collection("users").where({ openid }).get();
+        return {
+            code: 0,
+            message: '字段新增成功',
+            data:resn.data
         }
     }
 }
--- miniprogram/pages/user-center/index.js
+++ miniprogram/pages/user-center/index.js
@@ -9,23 +9,72 @@ Page({
     data: {
         defAvartarUrl: '../../images/icons/avatar.png',
         userInfo: {
-            avatarUrl: '',
+            userAvatar: '',
             nickName:''
         },
     },
     onLoad() {
         var that =this 
-        that.getUsetInfo()
+        that.getUserInfo()
     },
 
-    async getUsetInfo(){
+    async getUserInfo(){
+      let that = this
       let res = await wx.cloud.callFunction({
-        name: 'cl_userInfo',
-        data:{
-          type:'get'
-        }
+          name:'cl_userInfo',
+          data:{
+              type:'get'
+          }
       })
       console.log(res)
-    }
+      that.setData({
+          userInfo:res.result
+      })
+  },
+  onChooseAvatar(e){
+      let that = this
+      console.log(that.data.userInfo._id)
+      const avatarUrl = e.detail.avatarUrl;
+      this.uploadAvatarToCloud(avatarUrl);
+      
+  },
+  uploadAvatarToCloud(avatarUrl) {
+      wx.cloud.uploadFile({
+        cloudPath: `user_avatars/${Date.now()}.jpg`,
+        filePath: avatarUrl,
+        success: (res) => {
+          const fileID = res.fileID;
+          this.saveInfo('userAvatar',fileID)
+        },
+        fail: (err) => {
+          console.log('头像上传失败:', err);
+        }
+      });
+    },
+  onInputNickname(e){
+      let that = this
+      console.log(e.detail.value)
+      console.log(that.data.userInfo._id)
+      this.saveInfo('nickName',e.detail.value)
+  },
+
+  //保存用户名和头像
+  async saveInfo(types,values){
+      let that = this
+      let res = await wx.cloud.callFunction({
+          name:'cl_userInfo',
+          data:{
+              type:'post',
+              id:that.data.userInfo._id,
+              fields: {
+                  [types]:values
+              }
+          }
+      })
+      console.log(res.result.data[0])
+      that.setData({
+          userInfo:res.result.data[0]
+      })
+  },
 
 });
