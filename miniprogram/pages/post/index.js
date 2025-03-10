87fb1685550f9ca610dddc2cd733ca21c994d9fc
“tianchoy” <tianchoy@live.com>
2025-03-10 21:53:22 +0800

    3.10，船主端，已登陆，推荐模块，架构搭建完成

--- miniprogram/pages/post/index.js
+++ miniprogram/pages/post/index.js
@@ -1,66 +1,20 @@
-// pages/post/index.js
 Page({
+    data: {
+    },
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad(options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
-})
+    onLoad: function(options) {
+      // 页面创建时执行
+    },
+    onShow: function() {
+      // 页面显示时执行
+    },
+    onReady: function() {
+      // 页面首次渲染完毕时执行
+    },
+    onHide: function() {
+      // 页面隐藏时执行
+    },
+    onUnload: function() {
+      // 页面卸载时执行
+    }
+  });
