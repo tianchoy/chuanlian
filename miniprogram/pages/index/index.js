9e13cd519cbaf5f4995c310e7e2fe7111d0436b6
“tianchoy” <tianchoy@live.com>
2025-03-11 21:31:28 +0800

    3.11，获取地理位置

--- miniprogram/pages/index/index.js
+++ miniprogram/pages/index/index.js
@@ -20,70 +20,70 @@ Page({
         ],
         jobLists: [
             [
-              { title: '一类轮机长', salary: '8000', route: '上海~武汉', date: '2月10号' },
-              { title: '一类轮机长', salary: '8000', route: '上海~武汉', date: '2月10号' }
+                { title: '一类轮机长', salary: '8000', route: '上海~武汉', date: '2月10号' },
+                { title: '一类轮机长', salary: '8000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '二类轮机长', salary: '6000', route: '上海~武汉', date: '2月10号' },
-              { title: '二类轮机长', salary: '6000', route: '上海~武汉', date: '2月10号' }
+                { title: '二类轮机长', salary: '6000', route: '上海~武汉', date: '2月10号' },
+                { title: '二类轮机长', salary: '6000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '三类轮机长', salary: '5000', route: '上海~武汉', date: '2月10号' },
-              { title: '三类轮机长', salary: '5000', route: '上海~武汉', date: '2月10号' }
+                { title: '三类轮机长', salary: '5000', route: '上海~武汉', date: '2月10号' },
+                { title: '三类轮机长', salary: '5000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '一类轮船员', salary: '7000', route: '上海~武汉', date: '2月10号' },
-              { title: '一类轮船员', salary: '7000', route: '上海~武汉', date: '2月10号' }
+                { title: '一类轮船员', salary: '7000', route: '上海~武汉', date: '2月10号' },
+                { title: '一类轮船员', salary: '7000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '二类轮船员', salary: '6000', route: '上海~武汉', date: '2月10号' },
-              { title: '二类轮船员', salary: '6000', route: '上海~武汉', date: '2月10号' }
+                { title: '二类轮船员', salary: '6000', route: '上海~武汉', date: '2月10号' },
+                { title: '二类轮船员', salary: '6000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '三类轮船员', salary: '5000', route: '上海~武汉', date: '2月10号' },
-              { title: '三类轮船员', salary: '5000', route: '上海~武汉', date: '2月10号' }
+                { title: '三类轮船员', salary: '5000', route: '上海~武汉', date: '2月10号' },
+                { title: '三类轮船员', salary: '5000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '一类船长', salary: '10000', route: '上海~武汉', date: '2月10号' },
-              { title: '一类船长', salary: '10000', route: '上海~武汉', date: '2月10号' }
+                { title: '一类船长', salary: '10000', route: '上海~武汉', date: '2月10号' },
+                { title: '一类船长', salary: '10000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '二类船长', salary: '8000', route: '上海~武汉', date: '2月10号' },
-              { title: '二类船长', salary: '8000', route: '上海~武汉', date: '2月10号' }
+                { title: '二类船长', salary: '8000', route: '上海~武汉', date: '2月10号' },
+                { title: '二类船长', salary: '8000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '三类船长', salary: '6000', route: '上海~武汉', date: '2月10号' },
-              { title: '三类船长', salary: '6000', route: '上海~武汉', date: '2月10号' }
+                { title: '三类船长', salary: '6000', route: '上海~武汉', date: '2月10号' },
+                { title: '三类船长', salary: '6000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '一类水手', salary: '5000', route: '上海~武汉', date: '2月10号' },
-              { title: '一类水手', salary: '5000', route: '上海~武汉', date: '2月10号' }
+                { title: '一类水手', salary: '5000', route: '上海~武汉', date: '2月10号' },
+                { title: '一类水手', salary: '5000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '二类水手', salary: '4000', route: '上海~武汉', date: '2月10号' },
-              { title: '二类水手', salary: '4000', route: '上海~武汉', date: '2月10号' }
+                { title: '二类水手', salary: '4000', route: '上海~武汉', date: '2月10号' },
+                { title: '二类水手', salary: '4000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '三类水手', salary: '3000', route: '上海~武汉', date: '2月10号' },
-              { title: '三类水手', salary: '3000', route: '上海~武汉', date: '2月10号' }
+                { title: '三类水手', salary: '3000', route: '上海~武汉', date: '2月10号' },
+                { title: '三类水手', salary: '3000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '一类厨师', salary: '6000', route: '上海~武汉', date: '2月10号' },
-              { title: '一类厨师', salary: '6000', route: '上海~武汉', date: '2月10号' }
+                { title: '一类厨师', salary: '6000', route: '上海~武汉', date: '2月10号' },
+                { title: '一类厨师', salary: '6000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '二类厨师', salary: '5000', route: '上海~武汉', date: '2月10号' },
-              { title: '二类厨师', salary: '5000', route: '上海~武汉', date: '2月10号' }
+                { title: '二类厨师', salary: '5000', route: '上海~武汉', date: '2月10号' },
+                { title: '二类厨师', salary: '5000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '三类厨师', salary: '4000', route: '上海~武汉', date: '2月10号' },
-              { title: '三类厨师', salary: '4000', route: '上海~武汉', date: '2月10号' }
+                { title: '三类厨师', salary: '4000', route: '上海~武汉', date: '2月10号' },
+                { title: '三类厨师', salary: '4000', route: '上海~武汉', date: '2月10号' }
             ],
             [
-              { title: '其他岗位', salary: '5000', route: '上海~武汉', date: '2月10号' },
-              { title: '其他岗位', salary: '5000', route: '上海~武汉', date: '2月10号' }
+                { title: '其他岗位', salary: '5000', route: '上海~武汉', date: '2月10号' },
+                { title: '其他岗位', salary: '5000', route: '上海~武汉', date: '2月10号' }
             ]
-          ],
+        ],
         currentTab: 0, // 当前选中的 Tab
         scrollLeft: 0, // 选项卡滚动位置
         swiperHeight: 0, // swiper 的高度
@@ -100,6 +100,49 @@ Page({
         this.setData({
             swiperHeight: windowHeight - tabHeight,
             screenWidth: screenWidth
+        });
+        this.getLocation()
+    },
+
+    // 获取地理位置
+    getLocation() {
+        wx.getLocation({
+            type: 'wgs84',
+            success: (res) => {
+                const latitude = res.latitude;
+                const longitude = res.longitude;
+                this.setData({
+                    latitude,
+                    longitude
+                });
+                console.log(res)
+                // this.getAddress(latitude, longitude);
+            },
+            fail: (err) => {
+                if (err.errMsg === 'getLocation:fail auth deny') {
+                    wx.showModal({
+                        title: '提示',
+                        content: '请授权地理位置权限',
+                        success: (res) => {
+                            if (res.confirm) {
+                                wx.openSetting({
+                                    success: (res) => {
+                                        if (res.authSetting['scope.userLocation']) {
+                                            this.getLocation(); // 重新获取位置
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+                } else {
+                    console.error('获取地理位置失败', err);
+                    wx.showToast({
+                        title: '获取位置失败',
+                        icon: 'none'
+                    });
+                }
+            }
         });
     },
 
